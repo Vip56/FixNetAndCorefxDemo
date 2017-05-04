962307b974f3b2875af4d194b6ccc91237390336
姚正发 <976691141@qq.com>
2017-05-04 09:19:22 +0800

    Dockerfile增加字体安装，修正中文水印docker下无法打印问题

--- CryptoCorefx/ImageTest.cs
+++ CryptoCorefx/ImageTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Net.Http;
@@ -13,12 +14,13 @@ namespace CryptoCorefx
         public async Task Process()
         {
             HttpClient client = new HttpClient();
-            var stream = await client.GetStreamAsync("https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=2933773599,3484489584&fm=58");
+            var stream = await client.GetStreamAsync("http://img1.cache.netease.com/catchpic/3/30/30082E5F12205745CAC3105B8764984B.jpg");
 
-            int size = 150;
+            int size = 1024;
             int quality = 75;
 
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "image/test.jpg");
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "image");
+            path = Path.Combine(path, "test.jpg");
 
             using (var imgSource = Image.FromStream(stream))
             {
@@ -45,10 +47,23 @@ namespace CryptoCorefx
                         g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
                         g.DrawImage(imgSource, new Rectangle(0, 0, width, height), 0, 0, imgSource.Width, imgSource.Height, GraphicsUnit.Pixel);
 
-                        outMap.Save(path, ImageFormat.Jpeg);
+                        Font font = new Font("WenQuanYi Zen Hei", 18);
+                        SizeF fontSize = g.MeasureString("中文测试 test", font);
+
+                        StringFormat stringFormat = new StringFormat { Alignment = StringAlignment.Center };
+                        SolidBrush solodBrush = new SolidBrush(Color.FromArgb(220, 0, 0, 0));
+                        g.DrawString("中文测试 test", font, solodBrush, width - fontSize.Width, height - fontSize.Height, stringFormat);
+                        solodBrush.Dispose();
                     }
+
+                    outMap.Save(path, ImageFormat.Jpeg);
                 }
             }
+        }
+
+        public void DrawString()
+        {
+
         }
     }
 }
