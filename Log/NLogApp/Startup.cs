6b0001a20b71ea0a29cf199e7da8644b0e025c87
姚正发 <976691141@qq.com>
2017-06-28 14:54:28 +0800

    增加nlog demo

---
+++ Log/NLogApp/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog;
+using System.Text;
+
+namespace NLogApp
+{
+    public class Startup
+    {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+            loggerFactory.AddNLog();
+            loggerFactory.ConfigureNLog("nlog.config");
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                log.Debug("测试消息");
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
