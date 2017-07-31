46b0248fb8c31174d024585515c1b7fdcb8118a3
姚正发 <976691141@qq.com>
2017-07-31 09:08:43 +0800

    提交jsonnet测试

---
+++ JsonNet/ConsoleDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleDemo
+{
+    public class Program
+    {
+        public class DotoA
+        {
+            public DateTime Now { get; set; }
+
+            public Guid Id { get; set; }
+
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+
+            public List<int> Ages { get; set; }
+
+            public List<string> Names { get; set; }
+        }
+
+        public static void Main(string[] args)
+        {
+            DotoA a = new DotoA
+            {
+                Now = DateTime.Now,
+                Id = Guid.NewGuid(),
+                Name = "test1",
+                Age = 11,
+                Ages = new List<int>
+                {
+                    1,2,3,4,5,6,7,7,8,9,0
+                },
+                Names = new List<string>
+                {
+                    "1","2","3","4","5"
+                }
+            };
+
+            Console.WriteLine(JsonConvert.SerializeObject(a));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ JsonNet/ConsoleDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleDemo")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b949e35-6d6b-4b6f-90c9-9d3bc589aed3")]
